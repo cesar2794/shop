7fd9fb168b68dbf22fde03aba5663911096327f7
unknown <cesar.rosas2794@gmail.com>
2020-11-24 19:00:14 -0500

    Factorias y Faker para Categoria, Producto y ProductoImagen

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'cesar',
+            'email' => 'correo@correo.com',
+            'password' => bcrypt('12345678')
+        ]);
+
+        User::create([
+            'name' => 'augusto',
+            'email' => 'correo2@correo.com',
+            'password' => bcrypt('12345678')
+        ]);
+    }
+}
